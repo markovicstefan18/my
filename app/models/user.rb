3b9be6c373d09eccd37fa8dbd085de7df93f0722
Marković Stefan <markovicstefan@gmail.com>
2017-09-10 08:49:30 +0200

    added notice and styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
     attr_accessor :stripe_card_token
     
     def save_with_payment
-        if valid?
-            customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
-            self.stripe_customer_token = customer.id
-            save!
-        end
+    if valid?
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      self.stripe_customer_token = customer.id
+      save!
     end
+  end
 end
